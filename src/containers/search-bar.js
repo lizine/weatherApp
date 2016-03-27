bae43aca6533c662a1e032a1f8967a6a324a9cc2
Nina <nina.liljeblad@gmail.com>
2016-03-27 20:30:50 +0300

    search bar is now controlled form input with palceholder text

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 
 
 export default class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {term: ''}; //component level state, not redux level
+  }
   render(){
     return(
-      <div>
-      <input></input>
-      <button className="button">Search</button>
-      </div>
+      <form className="input-group">
+       <input
+       placeholder="Etsi sääennustetta haluamastasi kaupungista"
+       className="form-control"
+       value = {this.state.term}
+       onChange={event => this.setState({term:event.target.value})}
+       />
+       <span className="input-group-btn">
+        <button className="btn btn-secondary">Search</button>
+       </span>
+      </form>
     );
   }
 }
