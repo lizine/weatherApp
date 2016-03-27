d5ac677fdd906d3f1dd556ca5c99609d0bb2d57e
Nina <nina.liljeblad@gmail.com>
2016-03-27 22:06:32 +0300

    wrapped serachbar into a from

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -5,15 +5,23 @@ export default class SearchBar extends Component {
   constructor(props){
     super(props);
     this.state = {term: ''}; //component level state, not redux level
+    this.onInputChange = this.onInputChange.bind(this); //if you don bind, this.setstate wont work, it has wrong context
   }
+
+  onInputChange(event){
+  this.setState({term: event.target.value});
+  }
+onFormSubmit(event){
+  event.preventDefault();
+}
   render(){
-    return(
-      <form className="input-group">
+    return (
+      <form onSubmit={this.onFormSubmit}className="input-group">
        <input
        placeholder="Etsi sääennustetta haluamastasi kaupungista"
        className="form-control"
        value = {this.state.term}
-       onChange={event => this.setState({term:event.target.value})}
+       onChange={this.onInputChange}
        />
        <span className="input-group-btn">
         <button className="btn btn-secondary">Search</button>
