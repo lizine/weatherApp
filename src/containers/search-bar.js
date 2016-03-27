025bd4b80b678ef1fcd696f97e979d00d6a14320
Nina <nina.liljeblad@gmail.com>
2016-03-28 01:16:15 +0300

    searchbar is connected to app state now

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
     this.state = {term: ''}; //component level state, not redux level
     this.onInputChange = this.onInputChange.bind(this); //if you don bind, this.setstate wont work, it has wrong context
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
-  onInputChange(event){
+onInputChange(event){
   this.setState({term: event.target.value});
   }
+
 onFormSubmit(event){
   event.preventDefault();
+  //fetch weather data.
+  this.props.fetchWeather(this.state.term);
+  this.setState({term: ''}); //this changes only the component state
 }
   render(){
     return (
@@ -30,3 +37,7 @@ onFormSubmit(event){
     );
   }
 }
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather}, dispatch);
+}
+export default connect(null, mapDispatchToProps)(SearchBar);
