d7e745692a8c7ad89054027e7323bca512c8ff84
Nina <nina.liljeblad@gmail.com>
2016-03-27 20:01:38 +0300

    base containers for app

---
+++ src/containers/forecast-list.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default class ForecastList extends Component {
+  render() {
+    return(
+      <div>Forecastlist</div>
+    );
+
+  }
+}
---
+++ src/containers/search-bar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+export default class SearchBar extends Component {
+  render(){
+    return(
+      <div>
+      <input></input>
+      <button className="button">Search</button>
+      </div>
+    );
+  }
+}
