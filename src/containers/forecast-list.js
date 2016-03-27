1d3fff94833488fef8040222a34432ed8ed974bf
Nina <nina.liljeblad@gmail.com>
2016-03-28 01:14:19 +0300

    forecast list is tapped to state and shows information for one city

--- src/containers/forecast-list.js
+++ src/containers/forecast-list.js
@@ -1,10 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default class ForecastList extends Component {
+class ForecastList extends Component {
+  constructor(props){
+      super(props);
+
+  }
   render() {
+    if(this.props.weather.length === 0){
+      return(
+        <div>No Weather Data to show yet</div>
+      )
+    }
     return(
-      <div>Forecastlist</div>
+    //  <div>Forecast: {this.props.weather[0].list[0].main.temp}</div>
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{this.props.weather[0].city.name}</td>
+            <td>{this.props.weather[0].list[0].main.temp}</td>
+          <td>{this.props.weather[0].list[0].main.pressure}</td>
+          <td>{this.props.weather[0].list[0].main.humidity}</td>
+        </tr>
+      </tbody>
+    </table>
     );
 
   }
 }
+function mapStateToProps(state){
+  return {
+    weather: state.weather
+  };
+}
+export default connect(mapStateToProps)(ForecastList);
