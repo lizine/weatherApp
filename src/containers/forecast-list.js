82b631c621802556c5acc5022d0c690ece9677ac
Nina <nina.liljeblad@gmail.com>
2016-03-28 16:59:17 +0300

    took weather charts from this component to their own chart component

--- src/containers/forecast-list.js
+++ src/containers/forecast-list.js
@@ -1,45 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Chart from '../components/chart';
 
 class ForecastList extends Component {
+
+  renderWeather(cityData){
+    const temps = cityData.list.map(item => (item.main.temp-273.15));
+    console.log(temps);
+    const pressures = cityData.list.map(item => item.main.pressure);
+    const humidity = cityData.list.map(item => item.main.humidity);
+
+
+    return (
+      <tr key={cityData.city.name}>
+        <td>{cityData.city.name}</td>
+        <td><Chart data={temps} color="red" units="c"/> </td>
+        <td><Chart data={pressures} color="grey" units="hPa"/> </td>
+        <td> <Chart data={humidity} color="blue" units="%"/></td>
+      </tr>
+    )
+  }
   constructor(props){
       super(props);
 
   }
   render() {
-    if(this.props.weather.length === 0){
-      return(
-        <div>No Weather Data to show yet</div>
-      )
-    }
+
     return(
     //  <div>Forecast: {this.props.weather[0].list[0].main.temp}</div>
     <table className="table table-hover">
       <thead>
         <tr>
-          <th>City</th>
-          <th>Temperature</th>
-          <th>Pressure</th>
-          <th>Humidity</th>
+          <th>Kaupunki</th>
+          <th>Lämpötila (c)</th>
+          <th>Ilmanpaine (hPa)</th>
+          <th>Ilmankosteus (%)</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>{this.props.weather[0].city.name}</td>
-            <td>{this.props.weather[0].list[0].main.temp}</td>
-          <td>{this.props.weather[0].list[0].main.pressure}</td>
-          <td>{this.props.weather[0].list[0].main.humidity}</td>
-        </tr>
+        {this.props.weather.map(this.renderWeather)}
       </tbody>
     </table>
     );
 
   }
 }
-function mapStateToProps(state){
-  return {
-    weather: state.weather
-  };
+function mapStateToProps({weather}){
+  return { weather };
 }
 export default connect(mapStateToProps)(ForecastList);
