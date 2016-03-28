9bb07e91ebf453f55224a53ff015fde6e1819d53
Nina <nina.liljeblad@gmail.com>
2016-03-28 18:24:57 +0300

    fetching map from googlemaps wit lon and lat

--- src/containers/forecast-list.js
+++ src/containers/forecast-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class ForecastList extends Component {
 
@@ -10,16 +11,17 @@ class ForecastList extends Component {
     console.log(temps);
     const pressures = cityData.list.map(item => item.main.pressure);
     const humidity = cityData.list.map(item => item.main.humidity);
-
+    //ES6 magic, just pullinh lon and lat from data
+    const {lon, lat} = cityData.city.coord;
 
     return (
       <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
-        <td><Chart data={temps} color="red" units="c"/> </td>
+        <td>{cityData.city.name}<br/><GoogleMap lon={lon} lat={lat} /></td>
+        <td><Chart data={temps} color="red" units="°C"/> </td>
         <td><Chart data={pressures} color="grey" units="hPa"/> </td>
         <td> <Chart data={humidity} color="blue" units="%"/></td>
       </tr>
-    )
+    );
   }
   constructor(props){
       super(props);
@@ -33,7 +35,7 @@ class ForecastList extends Component {
       <thead>
         <tr>
           <th>Kaupunki</th>
-          <th>Lämpötila (c)</th>
+          <th>Lämpötila (°C)</th>
           <th>Ilmanpaine (hPa)</th>
           <th>Ilmankosteus (%)</th>
         </tr>
